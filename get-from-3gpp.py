51d98685456e16d32f2cd6798246726fa20b3b71
guillaume teissier <gte-itnsec@users.noreply.github.com>
2017-01-15 05:28:49 +0100

    Add copyrights header.

--- get-from-3gpp.py
+++ get-from-3gpp.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# Project     : diafuzzer
+# Copyright (C) 2017 Orange
+# All rights reserved.
+# This software is distributed under the terms and conditions of the 'BSD 3-Clause'
+# license which can be found in the file 'LICENSE' in this package distribution.
+
 import requests
 import zipfile
 from itertools import count
