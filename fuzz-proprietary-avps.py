a2cad90ee229ea5dd946d982de5e56411a0718a8
guillaume teissier <gte-itnsec@users.noreply.github.com>
2017-06-30 13:38:19 +0200

    make she-bangs consistent.

--- fuzz-proprietary-avps.py
+++ fuzz-proprietary-avps.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 # coding: utf8
 # Project     : diafuzzer
 # Copyright (C) 2017 Orange
--- fuzz.py
+++ fuzz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Project     : diafuzzer
 # Copyright (C) 2017 Orange
